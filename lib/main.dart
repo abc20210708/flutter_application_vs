30279065f3a91dd7723af04a848efdd06921e9f0
abc20210708 <gotqlc2670@naver.com>
2022-09-03 20:13:55 +0900

    강좌 8 | 플러터 앱페이지 기본 코드 이해하기 3

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "First app",
+      title: "First app_vs",
       theme: ThemeData(
-        primarySwatch: Colors.blue;
+        primarySwatch: Colors.blue
       ),
       home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("First app_vs"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Hello2"),
+            Text("Hello2"),
+            Text("Hello2")
+          ],
+        )),
     );
   }
 }
